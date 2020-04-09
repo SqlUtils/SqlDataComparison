d0e4a037b001f2fd13c2e341d889bb9e261359f1
MikeBeaton <mjsbeaton@gmail.com>
2020-04-09 16:59:29 +0100

    Some integration tests for other new bits (@ids, @where, @join, @use)

--- tests/test-dbs/RecreateTestTables.sql
+++ tests/test-dbs/RecreateTestTables.sql
@@ -22,6 +22,34 @@ INSERT INTO AddressTypes ( AddressType )
 VALUES ('Home')
 GO
 
+IF (object_id('CountryTable', 'U') IS NOT NULL)
+  DROP TABLE CountryTable
+GO
+
+CREATE TABLE [dbo].[CountryTable](
+    [CountryID] [int] NOT NULL,
+    [Country] [varchar](50) NOT NULL,
+    [Abbreviation] [varchar](10) NULL,
+    [CountryRegionID] [int] NOT NULL,
+    [Valid] [bit] NOT NULL,
+    CONSTRAINT [PK_CountryTable] PRIMARY KEY CLUSTERED 
+    (
+        [CountryID] ASC
+    )
+)
+GO
+
+INSERT INTO CountryTable (CountryID, Country, Abbreviation, CountryRegionID, Valid)
+VALUES
+  (1, 'Åland Islands', NULL, 9, 0),
+  (19, 'Barbados', NULL, 6, 1),
+  (158, 'Niue', NULL, 12, 0),
+  (159, 'Norfolk Island', NULL, 12, 0),
+  (230, 'United Kingdom', 'UK', 8, 1),
+  (231, 'United States', 'US', 11, 1),
+  (244, 'Zambia', NULL, 1, 1)
+GO
+
 /*
  * TEST DATABASE B
  */
@@ -79,4 +107,26 @@ GO
 
 INSERT INTO AddressTypes ( AddressType )
 VALUES ('Home'), ('Term'), ('Work')
+GO
+
+IF (object_id('CountryList', 'U') IS NOT NULL)
+  DROP TABLE CountryList
+GO
+
+CREATE TABLE [dbo].[CountryList](
+    [CountryID] [int] NOT NULL,
+    [Country] [nvarchar](50) NOT NULL,
+    CONSTRAINT [PK_CountryList] PRIMARY KEY CLUSTERED 
+    (
+        [CountryID] ASC
+    )
+)
+GO
+
+INSERT INTO CountryList (CountryID, Country)
+VALUES
+  (19, 'Barbados'),
+  (230, 'United Kingdom'),
+  (231, 'United States'),
+  (244, 'Zambia')
 GO
---
+++ tests/testUseWhereJoinIds.sql
@@ -0,0 +1,108 @@
+USE SqlUtils
+GO
+
+EXEC tSQLt.NewTestClass 'testUseWhereJoinIds';
+GO
+
+CREATE PROCEDURE testUseWhereJoinIds.[test @use works]
+AS
+BEGIN
+	DECLARE @CRLF CHAR(2) = CHAR(13) + CHAR(10)
+
+	DECLARE @Message NVARCHAR(MAX) =
+		'Data differences found between OURS <<< [SqlUtilsTests_A].[dbo].[CountryTable] and THEIRS >>> [SqlUtilsTests_C].[dbo].[CountryList].' + @CRLF +
+		' - Switch to results window to view differences.' + @CRLF +
+		' - Call [Import|Export][Added|Deleted|Changed|All] (e.g. ImportAdded) with the same arguments to transfer changes.' + @CRLF
+	EXEC tSQLt.ExpectException @Message, 16, 1
+
+	EXEC CompareData 'SqlUtilsTests_A..CountryTable', 'SqlUtilsTests_C..CountryList', @use='countryid, country'
+END
+GO
+
+CREATE PROCEDURE testUseWhereJoinIds.[test @use + @where works]
+AS
+BEGIN
+	DECLARE @CRLF CHAR(2) = CHAR(13) + CHAR(10)
+
+	EXEC tSQLt.CaptureOutput 'EXEC CompareData ''SqlUtilsTests_A..CountryTable'', ''SqlUtilsTests_C..CountryList'', @use=''countryid, country'', @where = ''[ours].valid = 1'''
+	SELECT CAST (
+		@CRLF +
+		'No data differences found between OURS <<< [SqlUtilsTests_A].[dbo].[CountryTable] and THEIRS >>> [SqlUtilsTests_C].[dbo].[CountryList].' + @CRLF
+		AS NVARCHAR(MAX)) AS OutputText
+	INTO #TestOutput
+
+	EXEC tSQLt.AssertEqualsTable '#TestOutput', 'SqlUtils.tSQLt.CaptureOutputLog'
+END
+GO
+
+CREATE PROCEDURE testUseWhereJoinIds.[test @use + ImportDeleted works]
+AS
+BEGIN
+	DECLARE @CRLF CHAR(2) = CHAR(13) + CHAR(10)
+
+	EXEC tSQLt.CaptureOutput 'EXEC ImportDeleted ''SqlUtilsTests_A..CountryTable'', ''SqlUtilsTests_C..CountryList'', @use=''countryid, country'''
+	SELECT CAST (
+		'' + @CRLF +
+		'Importing deleted rows...' + @CRLF +
+		'Requested import completed with no errors. Deleted 3 rows from [ours].' + @CRLF +
+		'' + @CRLF +
+		'No data differences found between OURS <<< [SqlUtilsTests_A].[dbo].[CountryTable] and THEIRS >>> [SqlUtilsTests_C].[dbo].[CountryList].' + @CRLF
+		AS NVARCHAR(MAX)) AS OutputText
+	INTO #TestOutput
+
+	EXEC tSQLt.AssertEqualsTable '#TestOutput', 'SqlUtils.tSQLt.CaptureOutputLog'
+END
+GO
+
+CREATE PROCEDURE testUseWhereJoinIds.[test @use + @ids + ImportDeleted works]
+AS
+BEGIN
+	DECLARE @CRLF CHAR(2) = CHAR(13) + CHAR(10)
+
+	EXEC tSQLt.CaptureOutput 'EXEC ImportDeleted ''SqlUtilsTests_A..CountryTable'', ''SqlUtilsTests_C..CountryList'', @use=''countryid, country'', @ids=''150-160'''
+	SELECT CAST (
+		'' + @CRLF +
+		'Importing deleted rows...' + @CRLF +
+		'Requested import completed with no errors. Deleted 2 rows from [ours].' + @CRLF +
+		'' + @CRLF +
+		'No data differences found between OURS <<< [SqlUtilsTests_A].[dbo].[CountryTable] and THEIRS >>> [SqlUtilsTests_C].[dbo].[CountryList].' + @CRLF
+		AS NVARCHAR(MAX)) AS OutputText
+	INTO #TestOutput
+
+	EXEC tSQLt.AssertEqualsTable '#TestOutput', 'SqlUtils.tSQLt.CaptureOutputLog'
+END
+GO
+
+CREATE PROCEDURE testUseWhereJoinIds.[test ImportChanged with all join columns works]
+AS
+BEGIN
+	DECLARE @CRLF CHAR(2) = CHAR(13) + CHAR(10)
+
+	EXEC tSQLt.CaptureOutput 'EXEC ImportChanged ''SqlUtilsTests_A..CountryTable'', ''SqlUtilsTests_C..CountryList'', @use=''countryid, country'', @show_sql = 1'
+	EXEC tSQLt.CaptureOutput 'EXEC ImportChanged ''SqlUtilsTests_A..CountryTable'', ''SqlUtilsTests_C..CountryList'', @use=''countryid, country'', @join=''countryid, country'', @show_sql = 1'
+
+	-- two rows
+	SELECT *
+	FROM SqlUtils.tSQLt.CaptureOutputLog
+
+	EXEC tSQLt.AssertEquals 2, @@ROWCOUNT
+
+	-- both did no updates
+	SELECT *
+	FROM SqlUtils.tSQLt.CaptureOutputLog
+	WHERE OutputText LIKE '%Updated 0 rows%'
+
+	EXEC tSQLt.AssertEquals 2, @@ROWCOUNT
+
+	-- second row has special SQL required for all join columns situation
+	DECLARE @id INT
+
+	SELECT @id = id
+	FROM SqlUtils.tSQLt.CaptureOutputLog
+	WHERE OutputText LIKE '%0 = 1%'
+
+	EXEC tSQLt.AssertEquals 1, @@ROWCOUNT
+	EXEC tSQLt.AssertEquals 2, @id
+
+END
+GO
