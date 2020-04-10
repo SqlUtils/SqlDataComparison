2247bec9c1c5071a7d3a27f28afed96111259a46
MikeBeaton <mjsbeaton@gmail.com>
2020-04-10 11:04:40 +0100

    Make DropTestDatabases.sql a separate file; also added as build o/p UninstallTestDbs.sql

---
+++ tests/test-dbs/CreateTestDatabases.sql
@@ -0,0 +1,29 @@
+/*
+ * TEST DATABASE A
+ */
+
+USE tempdb;
+GO
+
+CREATE DATABASE SqlUtilsTests_A
+GO
+
+/*
+ * TEST DATABASE B
+ */
+
+USE tempdb;
+GO
+
+CREATE DATABASE SqlUtilsTests_B
+GO
+
+/*
+ * TEST DATABASE C
+ */
+
+USE tempdb;
+GO
+
+CREATE DATABASE SqlUtilsTests_C
+GO
