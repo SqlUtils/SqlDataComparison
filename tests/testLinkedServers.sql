e9b48a11f31bd73add194b7f0b8d60ddaec21fdb
MikeBeaton <mjsbeaton@gmail.com>
2020-04-11 13:34:22 +0100

    Add linked server tests

---
+++ tests/testLinkedServers.sql
@@ -0,0 +1,65 @@
+USE master
+GO
+
+IF EXISTS(select * from sys.servers where is_linked = 1 and name = 'localhost')
+BEGIN
+	EXEC sp_dropserver 'localhost'
+END
+GO
+
+EXEC sp_addlinkedserver @server = 'localhost', @srvproduct = 'SQL Server'
+GO
+
+USE SqlUtils
+GO
+
+EXEC tSQLt.NewTestClass 'testLinkedServers';
+GO
+
+CREATE PROCEDURE testLinkedServers.[test illegal linked server for ours warning works]
+AS
+BEGIN
+	EXEC tSQLt.ExpectException @ExpectedMessagePattern = 'Could not find server ''xyz'' in sys.servers.%'
+	EXEC CompareData 'xyz.a..b', ''
+END
+GO
+
+CREATE PROCEDURE testLinkedServers.[test illegal linked server for theirs warning works]
+AS
+BEGIN
+	EXEC tSQLt.ExpectException @ExpectedMessagePattern = 'Could not find server ''xyz'' in sys.servers.%'
+	EXEC CompareData 'SQLUtilsTests_A..AddressTypes', 'xyz.a..b'
+END
+GO
+
+CREATE PROCEDURE testLinkedServers.[test illegal linked server table for ours warning works]
+AS
+BEGIN
+	EXEC tSQLt.ExpectException 'Cannot find database [a] on linked server [localhost]'
+	EXEC CompareData 'localhost.a..b', ''
+END
+GO
+
+CREATE PROCEDURE testLinkedServers.[test illegal linked server table for theirs warning works]
+AS
+BEGIN
+	EXEC tSQLt.ExpectException 'Cannot find database [a] on linked server [localhost]'
+	EXEC CompareData 'SQLUtilsTests_A..AddressTypes', 'localhost.a..b'
+END
+GO
+
+CREATE PROCEDURE testLinkedServers.[test no default sp_ database on ours when linked server]
+AS
+BEGIN
+	EXEC tSQLt.ExpectException 'Invalid or missing database name in parameter @ourTableName = ''localhost...AddressTypes'''
+	EXEC sp_CompareData 'localhost...AddressTypes', 'SQLUtilsTests_A..AddressTypes'
+END
+GO
+
+CREATE PROCEDURE testLinkedServers.[test no default sp_ database on theirs when linked server]
+AS
+BEGIN
+	EXEC tSQLt.ExpectException 'Invalid or missing database name in parameter @theirTableName = ''localhost...AddressTypes'''
+	EXEC sp_CompareData 'SQLUtilsTests_A..AddressTypes', 'localhost...AddressTypes'
+END
+GO
